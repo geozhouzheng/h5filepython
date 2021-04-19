51d5820c48fb5528b4885ff0f635299041efe41c
geozhouzheng <64129025+geozhouzheng@users.noreply.github.com>
2021-04-19 13:57:58 +0200

    Create animation.py
    ###############################################################################
    # animation.py is created for creating an animation for dynamic simulation    #
    # h5test.py should be run at first.                                           #
    # Created by Zheng Zhou 'zhougeohhu@foxmail.com'                              #
    # Please follow the explanation and edit it for your simulation result        #
    ###############################################################################

---
+++ animation.py
@@ -0,0 +1,51 @@
+###############################################################################
+# animation.py is created for creating an animation for dynamic simulation    #
+# h5test.py should be run at first.                                           #
+# Created by Zheng Zhou 'zhougeohhu@foxmail.com'                              #
+# Please follow the explanation and edit it for your simulation result        #
+###############################################################################
+
+import os
+import cv2
+import re
+# Python libraries above should be checked bedore your beginning
+# Python 3.X is suggested
+# If you use Spyder, you could have an interface similar to MatLAB
+ 
+picPath = '/root/msys_sandbox/BM/pics/'
+############################ animation generating #############################
+
+def get_images(path):
+    file_list = []
+    for root,dirs,files in os.walk(path):
+        if not files:
+            continue
+        for file in files:
+            if file.endswith('.png'):
+                file_list.append(os.path.join(root,file))
+    return file_list
+
+def key_sort(image_path):
+    pattern = re.compile("\d+")
+    image_name = os.path.basename(image_path)
+    return int(pattern.findall(image_name)[0])
+
+# Attention please, pictures are suggested to name by number for 'VideoWriter'
+# function could read pictures files in the actually orders.
+path        =   picPath
+file_list   =   get_images(path)
+file_list.sort(key=key_sort)
+# video basic setting
+fps             =   25
+img_size        =   (700,350)
+# folder and name for video
+save_path       =   '/root/msys_sandbox/BM/video/test.mp4'
+fourcc          =   cv2.VideoWriter_fourcc('M','P','N','G')
+video_writer    =   cv2.VideoWriter(save_path,fourcc,fps,img_size)
+for file_name in file_list:
+    print (file_name)
+    img = cv2.imread(file_name)
+    video_writer.write(img)
+
+video_writer.release()
+print ('finish')
