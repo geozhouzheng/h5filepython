425110ea0cd8ee56a1d31fd4225fe85ec9f6fc77
geozhouzheng <64129025+geozhouzheng@users.noreply.github.com>
2021-04-22 14:01:19 +0200

    Create basicH5operation.py
    some basic operation for understanding

---
+++ basicH5operation.py
@@ -0,0 +1,10 @@
+import mumpy as np
+import h5py
+# h5 file is a type of multiple dimensions file. In this file, data would be saved in 
+# different subsets. All the data are format as 1 dimensional array. If that data are multiple
+# dimensions, use numpy.array.reshape to reshape the array.
+
+f = h5py.File('filename', mode = 'r')
+# read h5 file
+print(f.key())
+# output the label of h5 file
